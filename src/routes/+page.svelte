81a644aaff34062f7d856fda60070d6f6f3c4b7b
jacksmedia <alex@j4cks.com>
2024-12-10 13:50:02 -0800

    📦 images & fonts added to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,37 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body>
+  <header>
+    <h1>
+      Jacks Media
+    </h1>
+    <h3>
+      Enthusiastic Guides for Digital Creatives
+    </h3>
+  </header>
+
+  <footer>
+
+  </footer>
+</body>
+
+
+<style>
+/* Fonts */
+/* new */
+@import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&family=Pixelify+Sans:wght@400;600&display=swap');
+
+h1, h2, h3, button {
+  font-family:  "Orbitron";
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: palegreen;
+}
+
+</style>
